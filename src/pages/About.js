dee026d8caf5a0dbc8cb726706588b2b349d99bc
Ulrik <umj4455@gmail.com>
2024-11-22 11:36:02 +0100

    Update branding: change project name and title to 'umj'

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      <h1 className='text-3xl'>About</h1>
+      <h1 className="text-3xl">About</h1>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
